52927e334374dea93732d85e93dbe66b88a06bad
QJin5566 <jinqi8869@hotmail.com>
2018-04-18 23:21:31 +1000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Component} from 'react';
+import Header from './components/Header';
+import {PageHeader} from 'react-bootstrap';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <PageHeader style={pageHeaderStyle}>
+          Cinema Aurora
+        </PageHeader>;
       </div>
     );
   }
 }
-
+let pageHeaderStyle = {
+  color: '#95afc0',
+  marginLeft: '3.75rem',
+  border:'none'
+}
 export default App;
