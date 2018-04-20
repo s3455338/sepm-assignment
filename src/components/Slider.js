0c92591b5a00e929418b94dcae5b90b3985a4af5
QJin5566 <jinqi8869@hotmail.com>
2018-04-20 16:56:24 +1000

    add pics

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import {Carousel} from 'react-bootstrap';
+import pic1 from '../assets/images/pic1.jpg'
+import pic2 from '../assets/images/pic2.jpg'
+import pic3 from '../assets/images/pic3.jpg'
+
 
 const Slider = () => (
   <Carousel>
     <Carousel.Item>
-      <img width={940} height={450} alt='940x450' style={{margin: 'auto'}} src="http://via.placeholder.com/940x450"/>
+      <img width={940} height={450} alt='940x450' style={{margin: 'auto'}} src={pic1}/>
       <Carousel.Caption>
         <h3>First slide label</h3>
         <p>COMMING SOON</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img width={940} height={450} alt='940x450' style={{margin: 'auto'}} src="http://via.placeholder.com/940x450"/>
+      <img width={940} height={450} alt='940x450' style={{margin: 'auto'}} src={pic2}/>
       <Carousel.Caption>
         <h3>Second slide label</h3>
         <p>NOW SHOWING</p>
       </Carousel.Caption>
     </Carousel.Item>
     <Carousel.Item>
-      <img width={940} height={450} alt='940x450' style={{margin: 'auto'}} src="http://via.placeholder.com/940x450"/>
+      <img width={940} height={450} alt='940x450' style={{margin: 'auto'}} src={pic3}/>
       <Carousel.Caption>
         <h3>Third slide label</h3>
         <p>NOW SHOWING</p>
